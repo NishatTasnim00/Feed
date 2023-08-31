5159593338707ba07c495c40e096cf6a507d0acc
NISHAT TASNIM <tasnim.nt00@gmail.com>
2023-09-01 03:38:04 +0600

    dwfw

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -17,6 +17,13 @@ const DashboardPage = () => {
 	} = useSWR('/api/posts', postFetcher, {
 		refreshInterval: 1000,
 	});
+
+	 const feedbackFetcher = (...args) => fetch(...args).then((res) => res.json());
+		const {
+			data: feedbacks,
+		} = useSWR('/api/posts', postFetcher, {
+			refreshInterval: 1000,
+		});
 console.log(posts)
 	return (
 		// <div className="flex gap-5 w-10/12 ">
@@ -33,7 +40,7 @@ console.log(posts)
 			</div>
 			<div className="w-80 h-64 glass shadow-xl rounded-lg flex justify-center items-center">
 				<p className="font-bold text-3xl font-sans ">
-					Total Feedback: {posts?.length}
+					Total Feedback: {feedbacks?.length}
 				</p>
 			</div>
 		</div>

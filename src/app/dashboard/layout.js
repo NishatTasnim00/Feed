044a865895977baebae175dcf89df980ceb529a6
NISHAT TASNIM <tasnim.nt00@gmail.com>
2023-08-31 13:57:36 +0600

    wr

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -6,13 +6,13 @@ const dashBoardLayout = ({ children }) => {
 	return (
 		<>
 			<div className="layout grid lg:grid-cols-12 z-50 ">
+				
 				<div className="grid col-span-1  mr-12 ">
 					<Sidebar></Sidebar>
 				</div>
 				<div className="grid col-span-11 z-10 ">
 					<TopBar></TopBar>
-					
-					
+
 					{children}
 				</div>
 

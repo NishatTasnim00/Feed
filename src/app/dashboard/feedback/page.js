5ba6c997ac0ef6484f60a0a26326262e9d663775
NISHAT TASNIM <tasnim.nt00@gmail.com>
2023-08-31 19:12:10 +0600

    fnfnw

---
+++ src/app/dashboard/feedback/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FeedbackPage = () => {
+  return (
+		<div className="w-10/12 mb-60 ml-auto mr-20 pt-8 mt-20 z-0"></div>
+	);
+}
+
+export default FeedbackPage
--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,11 +1,43 @@
-import React from 'react'
+'use client';
+import useSWR from 'swr';
 
 const DashboardPage = () => {
-  return (
-    <div>
-        
-    </div>
-  )
-}
+	const fetcher = (...args) => fetch(...args).then((res) => res.json());
+	const { data: users } = useSWR('/api/users', fetcher, {
+		refreshInterval: 1000,
+	});
+	console.log(users);
+
+
+  const postFetcher = (...args) => fetch(...args).then((res) => res.json());
+	const {
+		data: posts,
+		error,
+		isLoading,
+	} = useSWR('/api/posts', postFetcher, {
+		refreshInterval: 1000,
+	});
+console.log(posts)
+	return (
+		// <div className="flex gap-5 w-10/12 ">
+		<div className="w-10/12 mb-60 ml-auto mr-20 pt-8 mt-20 z-0 flex justify-between">
+			<div className="w-80 h-64 glass shadow-xl rounded-lg flex justify-center items-center">
+				<p className="font-bold font-sans text-3xl">
+					Total Users: {users?.length}
+				</p>
+			</div>
+			<div className="w-80 h-64 glass shadow-xl rounded-lg flex justify-center items-center">
+				<p className="font-bold text-3xl font-sans ">
+					Total Posts: {posts?.length}
+				</p>
+			</div>
+			<div className="w-80 h-64 glass shadow-xl rounded-lg flex justify-center items-center">
+				<p className="font-bold text-3xl font-sans ">
+					Total Feedback: {posts?.length}
+				</p>
+			</div>
+		</div>
+	);
+};
 
 export default DashboardPage;

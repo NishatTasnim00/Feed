730b7a6b5a219012f886c3d6b7c0680c276c12ff
Zarif <zarifraja19@gmail.com>
2023-08-31 12:56:10 +0600

    added

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -5,11 +5,11 @@ import React from 'react';
 const dashBoardLayout = ({ children }) => {
 	return (
 		<>
-			<div className="grid lg:grid-cols-12">
-				<div className="grid col-span-2">
+			<div className="layout grid lg:grid-cols-12 z-50 ">
+				<div className="grid col-span-1  mr-12 ">
 					<Sidebar></Sidebar>
 				</div>
-				<div className="grid col-span-10 ">
+				<div className="grid col-span-11 z-10 ">
 					<TopBar></TopBar>
 				</div>
 
--- src/components/Dashboard/Sidebar/Sidebar.jsx
+++ src/components/Dashboard/Sidebar/Sidebar.jsx
@@ -1,45 +1,111 @@
+/* eslint-disable jsx-a11y/alt-text */
+"use client"
+import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react'
-const Items = [
-	{
-		path: '/dashboard',
-		title: 'Dashboard',
-	},
-	{
-		path: '/dashboard/users',
-		title: 'Users',
-	},
+import React, { useState, useEffect, useRef, useCallback } from 'react';
+import { FaHome } from 'react-icons/fa';
+import { BsPersonWorkspace } from 'react-icons/bs';
+import { FaUsersGear } from 'react-icons/fa6';
+import { MdSpaceDashboard } from 'react-icons/md';
+import { TiThMenu } from 'react-icons/ti';
+import './sidebar.module.css';
 
-	{
-		path: '/dashboard/activities',
-		title: 'Activities',
-	},
-
-	{
-		path: '/dashboard',
-		title: 'Dashboard',
-	},
-];
 const Sidebar = () => {
+  const [isRotated, setIsRotated] = useState(false);
+
+const toggleLinks = () => {
+  setShowLinks(!showLinks);
+  setIsRotated(!isRotated); // Toggle the rotation state
+};
+  const [showLinks, setShowLinks] = useState(false);
+  const [menuRotation, setMenuRotation] = useState(0);
+  const menuRef = useRef(null);
+
+ 
+
+  const handleClickOutside = useCallback((event) => {
+    if (menuRef.current && !menuRef.current.contains(event.target)) {
+      setShowLinks(false);
+      setMenuRotation(menuRotation + 180); // Rotate back to the original position
+    }
+  }, [menuRotation]);
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [handleClickOutside]);
+
+  // ...
+
+
   return (
-		<>
-			<div className='min-h-screen bg-slate-400'
-
-            >
-                <h1>sidebar</h1>
-                <ul>{
-                    Items?.map(( {path, title} )=>(
-                        <li key={path}>
-                            <Link href={path}
-
-                            >{title}</Link>
-                        </li>
-                    ))
-                    }
-                </ul>
-            </div>
-		</>
-	);
-}
+    <>
+      <div
+        ref={menuRef}
+        className={`glass mx-auto min-h-screen  pl-5 relative pt-8 pr-12 ${
+          showLinks ? 'expanded' : ''
+        }`}
+      >
+      <TiThMenu
+  className={`cursor-pointer ml-6 menu-icon ${showLinks ? 'rotate' : ''}`}
+  size={30}
+  onClick={toggleLinks}
+  style={{
+    transform: `rotate(${isRotated ? 180 : 0}deg)`, // Rotate based on isRotated state
+  }}
+/>
+        <ul
+             className={`flex flex-col gap-8 mt-16 ml-6 ${
+              showLinks ? 'show' : ''
+            }`}
+        >
+          <li className={`link-item ${showLinks ? 'show' : ''}`}>
+            <Link href={"/"}>
+              <div className='flex gap-4'>
+                <FaHome size={26} />
+                {showLinks && <span>Home</span>}
+              </div>
+            </Link>
+          </li>
+          <li className={`link-item ${showLinks ? 'show' : ''}`}>
+            <Link href={"/dashboard/users"}>
+              <div className='flex gap-4'>
+                <FaUsersGear size={26} />
+                {showLinks && <span>Users</span>}
+              </div>
+            </Link>
+          </li>
+          <li className={`link-item ${showLinks ? 'show' : ''}`}>
+            <Link href={"/dashboard/activities"}>
+              <div className='flex gap-4'>
+                <BsPersonWorkspace size={26} />
+                {showLinks && <span>Activities</span>}
+              </div>
+            </Link>
+          </li>
+          <li className={`link-item ${showLinks ? 'show' : ''}`}>
+            <Link href={"/dashboard"}>
+              <div className='flex gap-4'>
+                <MdSpaceDashboard size={26} />
+                {showLinks && <span>Dashboard</span>}
+              </div>
+            </Link>
+          </li>
+        </ul>
+        <div className='absolute bottom-4 '>
+          <Image
+            className='mx-auto'
+            height={"55"}
+            width={"55"}
+            src={"https://i.ibb.co/34ZRTbb/swarm.png"}
+          />
+          <p className='text-center name'>Picxabee</p>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Sidebar;
